0796bd682e9ddc8d8a79f09afd56f5c637ec1bc8
jaden <jonghaha7@gmail.com>
2020-11-26 13:27:52 +0900

    init project

---
+++ src/main/java/com/spreadsheet/demospreadsheet/DemoSpreadsheetApplication.java
@@ -0,0 +1,13 @@
+package com.spreadsheet.demospreadsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpreadsheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpreadsheetApplication.class, args);
+	}
+
+}
